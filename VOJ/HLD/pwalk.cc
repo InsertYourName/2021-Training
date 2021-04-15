eb069a3d99979796fdb8f521b85160ec350d5aab
InsertYourName <82400255+InsertYourName@users.noreply.github.com>
2021-04-15 21:15:03 +0700

    Update pwalk.cc

--- VOJ/HLD/pwalk.cc
+++ VOJ/HLD/pwalk.cc
@@ -1,5 +1,3 @@
-// Draft !
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -21,7 +19,7 @@ struct Node {
 	Node(const vector<int>& vec, int lo, int hi) : low(lo), high(hi) {
 		if(low + 1 < high) {
 			int mid = (low + high) / 2;
-			l = new Node(vec, low, mid);
+			l = new Node(vec, low, mid); 
 			r = new Node(vec, mid, high);
 			value = f(l->value, r->value); // ?
 		} else {
@@ -40,8 +38,7 @@ struct Node {
 			mset = value = x, madd = 0;
 		} else {
 			push();
-			l->set(L, R, x);
-			r->set(L, R, x);
+			l->set(L, R, x), r->set(L, R, x);
 			value = f(l->value, r->value);
 		}
 	}
@@ -56,8 +53,7 @@ struct Node {
 			value += x;
 		} else {
 			push();
-			l->add(L, R, x);
-			r->add(L, R, x);
+			l->add(L, R, x), r->add(L, R, x);
 			value = f(l->value, r->value);
 		}
 	}
@@ -68,71 +64,50 @@ struct Node {
 			r = new Node(mid, high);		
 		}
 		if(mset != INF) {
-			l->set(low, high, mset);
-			r->set(low, high, mset);
+			l->set(low, high, mset), r->set(low, high, mset);
 			mset = INF; // ?
 		} else if(madd != 0) {
-			l->add(low, high, madd);
-			r->add(low, high, madd);
+			l->add(low, high, madd), r->add(low, high, madd);
 			madd = 0; // ?
 		}
 	}
-};
+} *tree;
 
-const bool VALS_EDGES = true;
+const bool VALS_EDGES = true; // ?
 
-const int N = (1 << 18);
+const int N = 100005;
 
-vector<int> adj[N];
-vector<pair<int, int>> cost[N];
+vector<pair<int, int>> adj[N]; // ?
 
-int par[N], siz[N], depth[N], rt[N], pos[N], timer = 0;
-
-Node* tree;
+int par[N], siz[N], depth[N], root[N], pos[N], timer = 0;
 
 int n;
 
-void dfs_siz(int v) {
-	if(par[v] != -1) {
-		auto it = find(adj[v].begin(), adj[v].end(), par[v]);		
-		adj[v].erase(it);		
-	}
-	for(int& u : adj[v]) {
-		par[u] = v;
-		depth[u] = depth[v] + 1;
-		dfs_siz(u);
-		siz[v] += siz[u];
-		if(siz[u] > siz[adj[v][0]]) {
-			swap(u, adj[v][0]); // ?
+/*
+int lca(int u, int v) {
+	for(; root[u] != root[v]; v = par[root[v]]) {
+		if(depth[root[u]] > depth[root[v]]) {
+			swap(u, v);
 		}
 	}
-}
-
-void dfs_hld(int v) {
-	pos[v] = timer++;
-	for(int u : adj[v]) {
-		rt[u] = (u == adj[v][0] ? rt[v] : u);
-		dfs_hld(u);
-	}
-}
-
-int lca(int u, int v) {
-	for(; rt[u] != rt[v]; v = par[rt[v]]) {
-		if(depth[rt[u]] > depth[rt[v]]) swap(u, v);
-	}
-	return depth[u] < depth[v] ? u : v;
+	return (depth[u] < depth[v]) ? u : v;
 }
 
 int dist(int u, int v) {
 	return depth[u] + depth[v] - 2 * depth[lca(u, v)];
 }
+*/
 
 template<class B> void process(int u, int v, B op) {
-	for(; rt[u] != rt[v]; v = par[rt[v]]) {
-		if(depth[rt[u]] > depth[rt[v]]) swap(u, v);
-		op(pos[rt[v]], pos[v] + 1);
+	for(; root[u] != root[v]; v = par[root[v]]) {
+		if(depth[root[u]] > depth[root[v]]) {
+			swap(u, v);
+		}
+		op(pos[root[v]], pos[v] + 1);
 	}
-	if(depth[u] > depth[v]) swap(u, v);
+	if(depth[u] > depth[v]) {
+		swap(u, v);
+	}
 	op(pos[u] + VALS_EDGES, pos[v] + 1);
 }
 
@@ -154,11 +129,30 @@ int query_subtree(int v) {
 	return tree->query(pos[v] + VALS_EDGES, pos[v] + siz[v]);
 }
 
-void dfs_cost(int v, int p = 0) {
-	for(const auto& [c, u] : cost[v]) {
-		if(u == p) continue;
+void dfs_siz(int v) {
+	if(par[v] != -1) {		
+		adj[v].erase(find_if(adj[v].begin(), adj[v].end(), [&](const pair<int, int>& ele) {
+			return ele.second == par[v];
+		}));		
+	}
+	for(auto& [c, u] : adj[v]) {
+		par[u] = v;
+		depth[u] = depth[v] + 1;
+		dfs_siz(u);
+		siz[v] += siz[u];
+		if(siz[u] > siz[adj[v][0].second]) {
+			swap(u, adj[v][0].second); // ?
+			swap(c, adj[v][0].first);
+		}
+	}
+}
+
+void dfs_hld(int v) {
+	pos[v] = timer++;
+	for(const auto& [c, u] : adj[v]) {
+		root[u] = (u == adj[v][0].second ? root[v] : u);			
+		dfs_hld(u);			
 		modify_path(v, u, c);
-		dfs_cost(u, v);
 	}
 }
 
@@ -169,10 +163,8 @@ int main() {
 		int u, v, c;
 		scanf("%d%d%d", &u, &v, &c);
 		--u, --v;		
-		adj[u].push_back(v);
-		adj[v].push_back(u);
-		cost[u].push_back({c, v});
-		cost[v].push_back({c, u});
+		adj[u].push_back({c, v});
+		adj[v].push_back({c, u});
 	}
 	tree = new Node(0, n);
 	for(int i = 0; i < n; ++i) {
@@ -181,7 +173,6 @@ int main() {
 	}
 	dfs_siz(0);
 	dfs_hld(0);	
-	dfs_cost(0);
 	while(q--) {
 		int u, v;
 		scanf("%d%d", &u, &v);
