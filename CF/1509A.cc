bc6ff25f9f89c763ab6ac5d6824e1fb615c9c944
InsertYourName <82400255+InsertYourName@users.noreply.github.com>
2021-04-16 21:03:34 +0700

    Create 1509A.cc

---
+++ CF/1509A.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 100005;
+int a[N], n;
+
+void solve() {
+	
+}
+
+int main() {	
+	int t;
+	scanf("%d", &t);
+	while(t--) {
+		solve();
+	}
+	return 0;
+}
