7f1b21315d7a76a9b26774865aca4c634dd05452
InsertYourName <82400255+InsertYourName@users.noreply.github.com>
2021-04-16 23:35:01 +0700

    Update 1509A.cc

--- CF/1509A.cc
+++ CF/1509A.cc
@@ -5,8 +5,28 @@ using namespace std;
 const int N = 100005;
 int a[N], n;
 
+int odd[N], even[N], p, q;
+
 void solve() {
-	
+	p = 0, q = 0;
+	scanf("%d", &n);
+	for(int i = 0; i < n; ++i) {
+		scanf("%d", &a[i]);
+	}
+	for(int i = 0; i < n; ++i) {
+		if(a[i] % 2 == 0) {
+			even[q++] = a[i];
+		} else {
+			odd[p++] = a[i];
+		}
+	}
+	for(int i = 0; i < q; ++i) {
+		printf("%d ", even[i]);
+	}
+	for(int i = 0; i < p; ++i) {
+		printf("%d ", odd[i]);		
+	}	
+	printf("\n");
 }
 
 int main() {	
