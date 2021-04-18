1a8f1493ad53438cddbda9df7e30f7a19262309a
InsertYourName <82400255+InsertYourName@users.noreply.github.com>
2021-04-18 19:14:03 +0700

    Update flexible.cc

--- algo.is/Lecture1/flexible.cc
+++ algo.is/Lecture1/flexible.cc
@@ -2,18 +2,7 @@
 
 using namespace std;
 
-const int N = 100005;
-int a[N], n;
-
-void solve() {
+int main() {
 	
-}
-
-int main() {	
-	int t;
-	scanf("%d", &t);
-	while(t--) {
-		solve();
-	}
 	return 0;
 }
